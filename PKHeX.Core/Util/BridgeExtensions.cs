63cfcc94d3cc59a10b5287d5fe61d72d650d1a6d
Cu3PO42 <cu3po42@gmail.com>
2018-05-03 17:15:36 +0200

    Add Array#ToArray as an identity function
    This appears to be neccessary since Bridge.NET returns Arrays where .Net
    doesn't

---
+++ PKHeX.Core/Util/BridgeExtensions.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace PKHeX.Core {
+    public static class BridgeExtensions {
+        public static dynamic ToArray(this Array a) {
+            return a;
+        }
+    }
+}
